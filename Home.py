14a69bee8aebc650ad06f46e826872cbb46e707d
Lyndon Tuala <lyndontuala28@gmail.com>
2024-11-19 15:59:37 +0800

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(layout="wide")
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/1.png")
+
+with col2:
+    st.title("Lyndon Tuala")
+    content = """
+    Hi!
+    """
+    st.info(content)
+
+st.write("Below you can find some of the apps I have built in Python. \
+         Feel free to cvontact me!")
+
+df = pd.read_csv("data.csv", sep=';')
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
