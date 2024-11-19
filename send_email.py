78b62a127a9b4e1a8f9bb7c881db7ba03560830d
Lyndon Tuala <lyndontuala28@gmail.com>
2024-11-19 16:11:49 +0800

    Create send_email.py

---
+++ send_email.py
@@ -0,0 +1,16 @@
+import smtplib, ssl
+import streamlit as st
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "lyndontuala@gmail.com"
+    password = st.secrets("PASSWORD")
+    receiver = "lyndontuala@gmail.com"
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
