4a034d6b35ca7c906754acc9ea0379796938f1d6
Lyndon Tuala <lyndontuala28@gmail.com>
2025-01-21 17:14:39 +0800

    Update homepage

--- Home.py
+++ Home.py
@@ -6,11 +6,13 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.image("images/github_dp.png")
-
+    st.write("Lyndon Tuala")
 with col2:
-    st.title("Lyndon Tuala")
+    st.title("A Glimpse into my Python Learning Journey")
     content = """
     Hi!
+    Here are some of the hands-on projects I created while learning Python through this  [Udemy course](https://www.udemy.com/course/the-python-mega-course/?course_id=692188).
+    Each project reflects the skills and concepts I explored along the way!
     """
     st.info(content)
 
@@ -27,7 +29,8 @@ with col3:
         st.write(row["description"])
         st.image("images/" + row["image"])
         st.write(f"[Source Code]({row['url']})")
-        st.write(f"[Demo]({row['demo']})")
+        if row['url'] != 'na':
+            st.write(f"[Demo]({row['demo']})")
 
 with col4:
     for index, row in df[10:].iterrows():
@@ -35,4 +38,5 @@ with col4:
         st.write(row["description"])
         st.image("images/" + row["image"])
         st.write(f"[Source Code]({row['url']})")
-        st.write(f"[Demo]({row['demo']})")
+        if row['url'] != 'na':
+            st.write(f"[Demo]({row['demo']})")
